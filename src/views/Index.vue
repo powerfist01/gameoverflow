5a19cc1160ac7617ef78db703aebbd9d32c868df
powerfist01 <sujeets207@gmail.com>
2020-07-28 21:55:59 +0530

    Updated
    Added cards

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,7 +4,7 @@
       <b-carousel
         id="carousel"
         v-model="slide"
-        :interval="4000"
+        :interval="3000"
         controls
         indicators
         background="#ababab"
@@ -12,18 +12,29 @@
         @sliding-start="onSlideStart"
         @sliding-end="onSlideEnd"
       >
-        <b-carousel-slide
-          :img-src="require('../assets/images/apex.jpg')"
-        ></b-carousel-slide>
+        <b-carousel-slide :img-src="require('../assets/images/apex.jpg')"></b-carousel-slide>
 
-        <b-carousel-slide
-          :img-src="require('../assets/images/csgo.jpg')"
-        ></b-carousel-slide>
+        <b-carousel-slide :img-src="require('../assets/images/csgo.jpg')"></b-carousel-slide>
 
-        <b-carousel-slide
-          :img-src="require('../assets/images/fifa.jpg')"
-        ></b-carousel-slide>
+        <b-carousel-slide :img-src="require('../assets/images/fifa.jpg')"></b-carousel-slide>
       </b-carousel>
+      <div class="cards">
+        <b-card-group deck>
+
+          <b-card
+            border-variant="secondary"
+            header="Secondary"
+            header-border-variant="secondary"
+            align="center"
+          >
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </b-card-text>
+          </b-card>
+
+          <b-card border-variant="success" header="Success" align="center">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </b-card-group>
+      </div>
     </b-container>
   </div>
 </template>
