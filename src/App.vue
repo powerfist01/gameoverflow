1f6fc09c77bbcde9387c3582c2e8bd7beda40a05
powerfist01 <sujeets207@gmail.com>
2020-08-27 20:45:52 +0530

    Updated the code

--- src/App.vue
+++ src/App.vue
@@ -27,24 +27,24 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form>
+          <b-nav-form inline>
             <b-form-input
               id="input"
               class="mr-sm-2"
               placeholder="Search..."
             ></b-form-input>
-            <router-link to="/login"
-              ><b-button class="mr-2 my-2 mr-sm-2" variant="outline-primary"
-                >Log in</b-button
-              ></router-link
-            >
-
-            <router-link to="/signup"
-              ><b-button class="mr-2 my-2 mr-sm-2" variant="primary"
-                >Sign up</b-button
-              ></router-link
-            >
           </b-nav-form>
+          <router-link to="/login"
+            ><b-button class="mr-2 my-2 mr-sm-2" variant="outline-primary"
+              >Log in</b-button
+            ></router-link
+          >
+
+          <router-link to="/signup"
+            ><b-button class="mr-2 my-2 mr-sm-2" variant="primary"
+              >Sign up</b-button
+            ></router-link
+          >
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -57,9 +57,6 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 export default {};
 </script>
 <style scoped>
-#input {
-  width: 500px;
-}
 .extra-routes {
   text-decoration: none;
 }
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,41 +1,57 @@
 <template>
   <div>
-    <b-container>
-      <b-carousel
-        id="carousel"
-        v-model="slide"
-        :interval="3000"
-        controls
-        indicators
-        background="#ababab"
-        img-height="360"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
-      >
-        <b-carousel-slide :img-src="require('../assets/images/apex.jpg')"></b-carousel-slide>
+    <b-carousel
+      id="carousel"
+      v-model="slide"
+      :interval="3000"
+      controls
+      indicators
+      background="#ababab"
+      img-height="100px"
+      img-width="100px"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide
+        :img-src="require('../assets/images/apex.jpg')"
+      ></b-carousel-slide>
 
-        <b-carousel-slide :img-src="require('../assets/images/csgo.jpg')"></b-carousel-slide>
+      <b-carousel-slide
+        :img-src="require('../assets/images/csgo.jpg')"
+      ></b-carousel-slide>
 
-        <b-carousel-slide :img-src="require('../assets/images/fifa.jpg')"></b-carousel-slide>
-      </b-carousel>
-      <div class="cards">
-        <b-card-group deck>
-
-          <b-card
-            border-variant="secondary"
-            header="Secondary"
-            header-border-variant="secondary"
-            align="center"
+      <b-carousel-slide
+        :img-src="require('../assets/images/fifa.jpg')"
+      ></b-carousel-slide>
+    </b-carousel>
+    <div class="cards">
+      <b-card-group deck>
+        <b-card border-variant="secondary" header="Secondary" align="center">
+          <b-card-text
+            >Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.</b-card-text
           >
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </b-card-text>
-          </b-card>
-
-          <b-card border-variant="success" header="Success" align="center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-          </b-card>
-        </b-card-group>
-      </div>
-    </b-container>
+        </b-card>
+        <b-card border-variant="secondary" header="Secondary" align="center">
+          <b-card-text
+            >Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.</b-card-text
+          >
+        </b-card>
+        <b-card border-variant="secondary" header="Secondary" align="center">
+          <b-card-text
+            >Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.</b-card-text
+          >
+        </b-card>
+        <b-card border-variant="secondary" header="Secondary" align="center">
+          <b-card-text
+            >Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit.</b-card-text
+          >
+        </b-card>
+      </b-card-group>
+    </div>
   </div>
 </template>
 
