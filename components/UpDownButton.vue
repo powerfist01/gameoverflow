f90e2cd0c8e3bbe6aade7662e67727c540495e8f
powerfist01 <sujeets207@gmail.com>
2021-04-24 15:33:14 +0530

    :zap: Updated the upvote/downvote component

--- components/UpDownButton.vue
+++ components/UpDownButton.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="text-center">
-    <div class="arrow-up"></div>
+    <div class="arrow-up" :class="[isUpvoted?'selected-top':'']" @click="upvote()"></div>
     {{ votes }}
-    <div class="arrow-down"></div>
+    <div class="arrow-down" :class="[isDownvoted?'selected-bottom':'']" @click="downvote()"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "UpDownButton",
+  data(){
+    return {
+      isUpvoted: true,
+      isDownvoted: false
+    }
+  },
   props:{
       votes: {
           type: Number,
           required: true
       }
   },
+  methods: {
+    async upvote(){
+      console.log("Upvotes");
+
+    },
+    async downvote(){
+      console.log(this);
+    }
+  }
 }
 </script>
 
@@ -34,5 +49,11 @@ export default {
   border-right: 15px solid transparent;
   
   border-top: 15px solid rgb(170, 160, 160);
+}
+.selected-top{
+  border-bottom: 15px solid rgb(224, 14, 77);
+}
+.selected-bottom{
+  border-top: 15px solid rgb(224, 14, 77);
 }
 </style>
