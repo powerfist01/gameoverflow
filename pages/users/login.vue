0f0509787dcbb657d92e49d979047a9635121f4f
powerfist01 <sujeets207@gmail.com>
2021-10-16 17:45:30 +0530

    :zap: Updating the login page

--- pages/users/login.vue
+++ pages/users/login.vue
@@ -1,46 +1,28 @@
 <template>
-  <div id="login">
-    <b-card bg-variant="light">
-      <b-form-group
-        label-cols-lg="3"
-        label="Log In Here"
-        label-size="lg"
-        label-class="font-weight-bold pt-0"
-        class="mb-0"
-      >
-        <b-form-group
-          label-cols-sm="3"
-          label="Username:"
-          label-align-sm="right"
-        >
-          <b-form-input placeholder="Username" id="username"></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          label-cols-sm="3"
-          label="Password:"
-          label-align-sm="right"
-        >
-          <b-form-input placeholder="Password" id="password"></b-form-input>
-        </b-form-group>
-
-        <b-form-group label-cols-sm="3" label-align-sm="right">
-          <b-button>Log In</b-button>
-        </b-form-group>
-
-        <div class="forgot-pass">
-          <b-form-group label-cols-sm="3">
-            <b-form-group
-              class="forgot"
-              label="Forgot Password?"
-              label-align-sm="left"
-            ></b-form-group>
-
-          </b-form-group>
+  <b-container>
+    <b-row>
+      <b-col cols="8">
+        <img
+          src="https://pixelz.cc/wp-content/uploads/2019/03/blue-planet-and-space-dust-wqhd-1440p-wallpaper.jpg"
+          class="login-image"
+        />
+      </b-col>
+      <b-col cols="4">
+        <div class="login-form">
+          <b-form-input
+            id="input-small"
+            size="sm"
+            placeholder="Username"
+          ></b-form-input>
+          <b-form-input
+            id="input-small"
+            size="sm"
+            placeholder="Password"
+          ></b-form-input>
         </div>
-      </b-form-group>
-    </b-card>
-  </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -49,29 +31,25 @@ export default {
   data() {
     return {};
   },
-  head(){
+  head() {
     return {
-      title: 'Log in - Game Overflow',
+      title: "Log in - Game Overflow",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
-          hid: 'gameoverflow',
-          name: 'Game Overflow',
-          content: 'Game Overflow - Where Gamers Meet'
-        }
-      ]
-    }
-  }
+          hid: "gameoverflow",
+          name: "Game Overflow",
+          content: "Game Overflow - Where Gamers Meet",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
-.forgot-pass {
-  width: 200px;
-  margin-left: 300px;
-}
-.forgot:hover {
-  cursor: pointer;
+.login-image {
+  width: 100%;
 }
 </style>
